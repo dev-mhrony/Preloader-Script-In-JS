bbc71b8f9fbd1d192c9e6a356b2a127f57d1cab3
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-11 00:29:57 +0600

    Last Update = 11-06-2022

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
     //     03. Here you have to take the name of the original div in the ID.
     //     04. It must be replaced by """preloader_main""".
 
-        var overlay = document.getElementById("preloader_main");
+        var overlay = document.getElementById("insert your div id name");
 
         window.addEventListener('load', function(){
             overlay.style.display = 'none';
